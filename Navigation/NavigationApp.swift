7598f6887181a5dec0b3eb642c9a47c6883cea5d
Ashley <anm38060@gmail.com>
2025-05-25 13:25:10 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Scholar on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
